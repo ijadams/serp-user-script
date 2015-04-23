1e3f829608240f35ec443bae96699a3c9fedf3fc
Ian Adams <ijadams503@gmail.com>
2015-04-23 15:15:13 -0500

    Fixed local storage function and added key event

--- contentscript.js
+++ contentscript.js
@@ -1,43 +1,56 @@
-$(document).ready(function(){
-   var domain = "";
-
-   var placeholder = localStorage(domain);
-   		if (placeholder != "") {
-   			 domain = window.prompt('What domain are you looking for?', placeholder);
-   			    localStorage['domain'] = domain;
-
-   		} else {
-   			 domain = window.prompt('What domain are you looking for?', 'example.com');
-   			    localStorage['domain'] = domain;
-
-   		}
-   window.alert(get_serp(domain));
+var down = [];
+$(document).keydown(function(e) {
+   down[e.keyCode] = true;
+}).keyup(function(e) {
+   if (down[17] && down[82]) {
+       main()
+   }
+   down[e.keyCode] = false;
 })
 
 
 function get_serp(hostname) {
-  //declare hostname to search for
-  var hostname = hostname;
-  //Get all HTML DOMs for domain names
-  results = document.getElementsByClassName('_Rm');
-  //See if what page user is searching on
-  start_match = document.location.href.match(/start=(\d+)/);
-       if(start_match) {
-             //Accounting for start page
-             start = parseInt(start_match[1]);
-            } else {
-             start = 0;
-            }
-  //loop through results
-  for (i=0;i<results.length;i++) {
-    //convert html dom to string
-    text_result=results[i].textContent;
-      //search string for hostname
-      if (text_result.search(hostname) != -1) {
-          return i+start+1;
-      } else {
-         /* not in the array */
-         return "Not found";
-      }
+ //declare hostname to search for
+ var hostname = hostname;
+ //Get all HTML DOMs for domain names
+ results = document.getElementsByClassName('_Rm');
+ //See if what page user is searching on
+ start_match = document.location.href.match(/start=(\d+)/);
+      if(start_match) {
+            //Accounting for start page
+            start = parseInt(start_match[1]);
+           } else {
+            start = 0;
+           }
+ //loop through results
+ var answer = 'not found'
+ for (i=0;i<results.length;i++) {
+   //convert html dom to string
+   text_result=results[i].textContent;
+     //search string for hostname
+     if (text_result.search(hostname) != -1) {
+         return answer = i+start+1;
+     }
+     	//not found
+ } return answer;
+}
 
-  }}
+
+function main(){
+         var domain = "";
+
+          var placeholder = window.localStorage.getItem('domain');
+             if (placeholder != "") {
+                domain = window.prompt('What domain are you looking for?', placeholder);
+                   localStorage['domain'] = domain;
+                             window.alert(get_serp(domain));
+
+
+             } else {
+                domain = window.prompt('What domain are you looking for?', 'example.com');
+                   localStorage['domain'] = domain;
+                             window.alert(get_serp(domain));
+
+
+             }
+        }
